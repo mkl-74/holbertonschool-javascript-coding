8bb30d1bb09556e29b96b5dcb111ac454ebe208d
mkl-74 <mickael1008@gmail.com>
2024-05-21 09:25:43 +0000

    task5

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -1,32 +1,12 @@
-#!/usr/bin/env node
+#!/usr/bin/node
 
 const request = require('request');
 const fs = require('fs');
 
-const url = process.argv[2];
-const filePath = process.argv[3];
-
-if (!url || !filePath) {
-  console.error('Usage: ./5-request_store.js <URL> <file path>');
-  process.exit(1);
-}
-
-request(url, (error, response, body) => {
-  if (error) {
-    console.error(`Error fetching URL: ${error}`);
-    return;
-  }
-
-  if (response.statusCode !== 200) {
-    console.error(`Failed to fetch URL. Status code: ${response.statusCode}`);
-    return;
-  }
-
-  fs.writeFile(filePath, body, 'utf8', (err) => {
+request(process.argv[2], function (_err, _res, body) {
+  fs.writeFile(process.argv[3], body, 'utf8', function (err) {
     if (err) {
-      console.error(`Error writing to file: ${err}`);
-      return;
+      console.log(err);
     }
-    console.log(`Content saved to ${filePath}`);
   });
 });
